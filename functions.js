da0c9e2ad9c9a01118c74c033757342116f17f66
DomiSchr <domischroeder@gmx.de>
2019-11-06 15:01:50 +0100

    JSON fehlt, evtl. passt aber Anbindung

--- functions.js
+++ functions.js
@@ -1,3 +1,35 @@
-function getProducts(){
-    alert("Es geht!");
+function getProducts()
+{
+alert("getProducts");
+	var url = "serverREST.php";
+	var method = "action=GET";
+	url += "?"+method;
+
+	var request = new XMLHttpRequest();
+	request.open("GET", url);
+	request.onload = function()
+	{
+		if(request.status == 200)
+		{
+			var stationlist = request.responseText;
+
+
+                //getTable header for data
+				var url2 = "includes/stationtable.json";
+	            var request2 = new XMLHttpRequest();
+	            request2.open("GET", url2);
+	            request2.onload = function()
+	            {
+		           if(request2.status == 200)
+		           {
+			           var stationtable = request2.responseText;
+			           listStation(stationlist,stationtable);
+		            }
+	            };
+	            request2.send(null);
+
+			
+		}
+	};
+	request.send(null);
 }
